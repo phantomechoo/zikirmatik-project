81afb27180e8b3904250a29ace7bc6962882eea2
Sena <126852452+phantomechoo@users.noreply.github.com>
2025-09-06 20:28:58 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const cacheName = 'tasbeehly-cache-v1';
+const filesToCache = [
+  './',
+  './index.html',
+  './resim.svg',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
